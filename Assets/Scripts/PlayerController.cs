e5af586aa9b645b4b367ba4605cf1f44c7881653
masterlopez <javy7@hotmail.com>
2024-03-09 01:09:02 -0400

    Added turning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,9 +11,14 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
+    public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
